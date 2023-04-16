e84a2ba1e1ce5e0b4624d75a469f6b44b28d748a
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-04-16 23:56:44 +0530

    added Heading and Paragraph section

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,31 +1,22 @@
 import Banner from "@/components/Banner"
+import Wrapper from "@/components/Wrapper"
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, dolores beatae quaerat ad esse, laboriosam facere pariatur reiciendis, vitae debitis asperiores hic aliquid dignissimos quam porro cum quasi quisquam consequatur.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, illo id atque earum tempora, velit eaque magni eos odit ipsa, dolor quasi nobis est maxime fugiat numquam magnam doloribus sapiente.
-        Dolore esse aut atque voluptates doloremque laudantium dignissimos facere sequi velit consequuntur soluta, nihil quia laboriosam eius tenetur magnam non, dicta ipsum sint excepturi quaerat? Eveniet vel tempore veniam laudantium.
-        Ipsum ducimus eius maxime veritatis autem explicabo consequuntur illum odit expedita! Repellat ad, aliquid sint quia nostrum, laboriosam alias reiciendis modi quam nam quisquam aut, id eum dignissimos adipisci recusandae.
-        Quisquam, ullam! Beatae voluptatibus deserunt, blanditiis ab possimus exercitationem perferendis accusantium iste ratione voluptate doloremque aperiam sunt deleniti nostrum odio, cum similique fugit, est totam! Odio reiciendis ut placeat architecto!
-        Sapiente, aliquam dignissimos repellat perferendis voluptatibus magni libero deleniti doloribus et minus accusamus quisquam quod officiis aspernatur repudiandae necessitatibus provident molestiae assumenda. Debitis, at! Iste, accusamus voluptate. Nihil, perferendis voluptatem.
-        Numquam dolorem earum placeat fugiat qui excepturi ipsam optio maiores vitae perferendis a dolore quam necessitatibus blanditiis, minima at beatae deserunt. Quaerat reiciendis libero harum fugit possimus quibusdam, optio soluta?
-        Blanditiis odio est quos ut vitae consequatur quis facilis. Id, doloribus. Ut modi laborum at nostrum voluptatem. Maiores minus odio ab earum qui repellendus. Id eum recusandae dignissimos voluptatem eius!
-        In ut voluptates accusamus recusandae quidem! Dolore atque dolor ex similique tempore laudantium labore eos asperiores harum recusandae quam dignissimos, dolorem, architecto natus illo ut quas temporibus error eum eligendi!
-        Suscipit corporis optio ipsa fugiat, aspernatur rem odit quaerat ratione reiciendis perspiciatis aut voluptatem ipsam laboriosam itaque ipsum laudantium asperiores tenetur odio cum architecto mollitia corrupti. Veritatis odit id ut.
-        Voluptates perferendis rem neque placeat unde et debitis illum cumque, deleniti porro repellendus? Tempora molestias soluta harum, non recusandae quasi porro eaque cum architecto voluptate ipsa asperiores voluptatibus obcaecati alias?
-        Voluptates commodi labore numquam, tempora, laborum excepturi, animi cupiditate in neque quod autem enim deleniti error velit architecto voluptatum obcaecati molestias temporibus eveniet! Optio quaerat aliquid quia excepturi eos natus.
-        Omnis ad, sunt fugit magni laborum id quibusdam veniam inventore repellat autem a vero dolorum! Nemo repellendus maxime praesentium repellat odit perspiciatis voluptatum molestias iusto, sapiente iste animi repudiandae quisquam.
-        Sint debitis quaerat veniam, quo tenetur ea explicabo fuga quidem autem suscipit quibusdam! Sequi ratione, quo cumque porro perspiciatis dicta suscipit iure laboriosam quaerat soluta! Iusto iure architecto magnam eveniet.
-        Aliquam, esse tempore hic expedita, minus harum modi sequi amet ipsum ex incidunt inventore debitis rerum eaque voluptatibus corporis quam sed, culpa voluptates cumque nulla optio. Dicta cumque eligendi explicabo.
-        Nobis velit dolore in eos cumque rem sint. Impedit sequi illo, possimus nisi natus voluptatum, facilis dignissimos cumque quasi deleniti labore modi voluptatem necessitatibus aut ducimus eligendi saepe quo eveniet!
-        Accusantium neque veniam voluptates fuga aut distinctio, nihil possimus deserunt quidem! Et similique eius quod dicta autem accusantium, ea dignissimos necessitatibus quam labore praesentium? Vitae totam tenetur veritatis maxime sed.
-        Iusto animi totam corporis quibusdam iste sapiente error nulla beatae. Voluptatum aliquam magni omnis vel explicabo numquam cupiditate, consequuntur eum error quae repellendus, fuga asperiores, exercitationem similique quis optio neque!
-        Repellat, voluptatem libero! Quia ratione perferendis consequatur ipsam eaque officiis maxime totam, voluptate quam dolore nesciunt delectus, animi ipsum facere sequi illo velit dolores. Architecto eligendi amet culpa optio praesentium.
-        Itaque totam tempore natus, asperiores ipsa dolore sequi officiis dolorum praesentium perferendis, voluptatum soluta, sapiente quidem? Ullam quod ratione deserunt soluta, porro, obcaecati corrupti, minima voluptas delectus libero minus dolor?
-      </p>
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] font-semibold leading-tight">
+            Cushioning for Your Miles
+          </div>
+          <div className="text-md md:text-lg">
+            A lightweight Nike ZoomX midsole is combined with
+            increased stack heights to help provide cushioning
+            during extended stretches of running.
+          </div>
+        </div>
+      </Wrapper>
     </main>
   )
 }
